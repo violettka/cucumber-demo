ed6dc0039bf7b55f6927e6fe4ed3879d87bfd39a
kikimorella <kikimorella@gmail.com>
2022-11-27 16:56:00 +0100

    fix: change HomepageSteps -> HomePageSteps

--- src/test/java/wolt/steps/HomepageSteps.java
+++ src/test/java/wolt/steps/HomepageSteps.java
@@ -2,50 +2,51 @@ package wolt.steps;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.junit.Assert;
-
-import java.util.List;
+import io.cucumber.java.en.When;
+import wolt.pages.Homepage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static wolt.Constants.BASE_URL;
 
 /**
- * Class contains all the steps that can be done from the Homepage.
+ * Class HomePgeSteps contains all base steps.
  */
-public class HomepageSteps extends StepsBase {
-
-    @Given("I open Wolt Homepage")
-    public void iOpenADiscoveryPage() {
-        open(BASE_URL);
-        homepage.accCookies();
-        homepage.homePageTextExist();
-    }
+public class HomePageSteps extends StepsBase {
 
     @Then("I should see Homepage")
     public void iShouldSeeHomepage() {
         homepage.homePageTextExist();
     }
 
-    @Then("I choose {} in {}")
-    public void iChooseCityInCountry(String city, String country) {
-        if (!homepage.checkForLocalCountry().equals(country)) {
-            homepage.showAllCountriesBtnClick();
-            homepage.clickOnCountryName(country);
-        }
-        discoveryPage = homepage.clickOnCityName(city);
+    @When("I click All countries link")
+    public void iClickAllCountriesLink() {
+        homepage.clickAllCountriesLink();
     }
 
-    @Then("I choose {} pointer")
-    public void iChooseCityName(String city) {
-        List<String> cityNames = homepage.getCityNames();
-        Assert.assertTrue(cityNames.contains(city));
-        discoveryPage = homepage.clickOnCityName(city);
+    @Then("I see list of countries")
+    public void iSeeListOfCountries() {
+        homepage.checkAllCountriesList();
     }
 
-    @Then("I choose {} button")
-    public void iChooseCountryName(String country) {
-        List<String> countryNames = homepage.getCountryNames();
-        Assert.assertTrue(countryNames.contains(country));
-        homepage.clickOnCountryName(country);
+    @When("I click {} country option")
+    public void iClickCountryOption(String country) {
+        homepage.getCountryOption(country);
+    }
+
+    @Then("I see list of cities in {}")
+    public void iSeeListOfCities(String country) {
+        homepage.checkListOfCities(country);
+    }
+
+    @When("I click {} city option")
+    public void iClickCityOption(String city) {
+        discoveryPage = homepage.clickCityOption(city);
+    }
+
+    @Given("I am on the Homepage")
+    public void iAmOnTheHomepage() {
+        open(BASE_URL);
+        homepage = new Homepage();
+        homepage.accCookies();
     }
 }
